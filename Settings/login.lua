de5ad5829f5f066e6a4fdee6e2759dbf594e7d7c
Fludex <71903277+Woutt@users.noreply.github.com>
2022-03-03 20:46:20 +0100

    Update login.lua

--- Settings/login.lua
+++ Settings/login.lua
@@ -1,30 +1,57 @@
 local table = {
-    [1] = "u1t0h3y2b9s9l9o5b2g1b5b5o7m0t9i4y3n8h4y5s2y1i1s3z2", --//test
+    [1] = "", --//test
     [2] = "o[tVTzrSQq]", --//op-slan in exploit
     [3] = "1865734982258", --//allen numbers
-    [4] = "https://controlc.com/1046025f", --//key website
+    [4] = "https://linktr.ee/Woutt",
+
     ["Get_Script"] = function() 
-        if game.PlaceId == 5445504800 or game.PlaceId == 4042427666 or game.PlaceId == 5113678354 or game.PlaceId == 5113680396 or game.PlaceId == 5445525505 or game.PlaceId == 6479720355 then 
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/Woutt/FludexHub/main/Games/Anime_Fighting_Sim.lua"))()
-        elseif game.PlaceId == 2202352383 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/Woutt/FludexHub/main/Games/Super_Power_Training_Sim.lua"))()
-        elseif game.PlaceId == 1 then
-            loadstring(game:HttpGet(""))()
-        else
-            print("no game found")
+        local afs = {
+            ["basic"] = {
+                5445504800, --Tournament-Dimension
+                4042427666, --Dimension-1
+                5113678354, --Dimension-2
+                5113680396, --Dimension-3
+                5445525505, --Dimension-4
+                6479720355, --Dimension-5
+            },
+
+            ["dungeons"] = {
+                7220975160, --Dungeon-1
+                7220986579, --Dungeon-2
+            },
+
+            ["bosses"] = {
+                6479693773, --Inferno
+                7220992373, --Akaza
+                8436368941, --Rimumu-Boss
+                6138035804, --Christmas-Boss
+            },
+        }
+
+        if table.find(afs["basic"], game.PlaceId) then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Woutt/FludexHub/main/Games/AFS_basic.lua"))()
+        elseif table.find(afs["dungeons"], game.PlaceId) then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Woutt/FludexHub/main/Games/AFS_dungeons.lua"))()
+        elseif table.find(afs["bosses"], game.PlaceId) then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Woutt/FludexHub/main/Games/AFS_bosses.lua"))()
+        elseif 2202352383 == game.PlaceId then 
+            loadstring(game:HttpGet(""))() --super power trainign sim
         end
     end,
+
     ["image"] = {
-    	["background"] = "http://www.roblox.com/asset/?id=8211657640",
-    	["glow"] = "http://www.roblox.com/asset/?id=8198465912",
-    	["copy"] = "http://www.roblox.com/asset/?id=8211721462",
-    	["load"] = "http://www.roblox.com/asset/?id=8197932333",
-    	["lock"] = "http://www.roblox.com/asset/?id=8198617824",
+    	["background"] =    "http://www.roblox.com/asset/?id=8211657640",
+    	["glow"] =          "http://www.roblox.com/asset/?id=8198465912",
+    	["copy"] =          "http://www.roblox.com/asset/?id=8211721462",
+    	["load"] =          "http://www.roblox.com/asset/?id=8197932333",
+    	["lock"] =          "http://www.roblox.com/asset/?id=8198617824",
     },
+
     ["discord"] = {
-    	["inv"] = "tg9uymesSZ",
+    	["inv"] =   "tg9uymesSZ",
     	["owner"] = "Woutt#4106",
     },
+
     ["get_exploit"] = function()
         local checker =
     	--god exploits
@@ -52,4 +79,5 @@ local table = {
         return checker
     end
 }
+
 return table
