49180f33540eb71892ab691e1854f894c5f925cb
Woutt <71903277+Woutt@users.noreply.github.com>
2022-08-04 20:57:46 +0200

    Update setup.lua

--- Libex/setup.lua
+++ Libex/setup.lua
@@ -20,9 +20,9 @@ end
 local GetAsset = function(x)
     local id = tbl[x]
     local AssetFunc = getsynasset or getcustomasset
-    if AssetFunc ~= nil then
-        return AssetFunc("FludexHub/Assets/"..id..".png")
-    else
+    -- if AssetFunc ~= nil then
+    --     return AssetFunc("FludexHub/Assets/"..id..".png")
+    -- else
         return "rbxasset://"..id
     end
 end
