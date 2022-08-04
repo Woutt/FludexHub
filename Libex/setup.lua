dfc0b4d9b110ba56dbc7b37825afa1a9b6a8c694
Woutt <71903277+Woutt@users.noreply.github.com>
2022-08-04 20:24:55 +0200

    Update setup.lua

--- Libex/setup.lua
+++ Libex/setup.lua
@@ -1,13 +1,18 @@
 local tbl = {
-    "10475785213", -- // circel
-    "10475787657", -- // square
-    "10475788806"  -- // shadow
+    "10475785213", -- // Circel
+    "10475787657", -- // Square
+    "10475788806", -- // Shadow
+    "10477330253", -- // Full Circel
+    "10477389259", -- // Gradient Plate
+    "10477426603"  -- // Scroll Square
 }
 
 for i,v in pairs(tbl) do
     if getasset ~= nil and writefile ~= nil then
         local url = "https://raw.githubusercontent.com/Woutt/FludexHub/main/Libex/Assets/10475785213.png"
-        writefile("FludexHub/Assets/"..v..".png", game:HttpGet(url))
+        if isfile("FludexHub/Assets/"..v..".png") then
+            writefile("FludexHub/Assets/"..v..".png", game:HttpGet(url))
+        end
     end
 end
 
