84e0f6e0eba84746d0289c43721aac40458c504a
Woutt <71903277+Woutt@users.noreply.github.com>
2022-08-04 20:37:50 +0200

    Update setup.lua

--- Libex/setup.lua
+++ Libex/setup.lua
@@ -8,10 +8,11 @@ local tbl = {
 }
 
 for i,v in pairs(tbl) do
-    if getasset ~= nil and writefile ~= nil then
-        if isfile("FludexHub/Assets/"..v..".png") then
+    if (writefile and isfile) then
+        if not isfile("FludexHub/Assets/"..v..".png") then
             local url = "https://raw.githubusercontent.com/Woutt/FludexHub/main/Libex/Assets/"..v..".png"
             writefile("FludexHub/Assets/"..v..".png", game:HttpGet(url))
+            print("sss")
         end
     end
 end
