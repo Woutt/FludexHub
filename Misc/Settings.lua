88e0280b48ddba4150357e465bd156ed61a435de
Woutt <71903277+Woutt@users.noreply.github.com>
2022-08-19 20:14:44 +0200

    Update Settings.lua

--- Misc/Settings.lua
+++ Misc/Settings.lua
@@ -1,13 +1,13 @@
 return {
-    [1] = "Lz8d8e7mURO2ncjeEz0H",
+    [1] = "Lz8d8e7mURO2ncjeEz0H28d4f6531dd0f30350e6423bb52ace43932b8159",
     [2] = "https://linktr.ee/Woutt",
 
-    ["discord"] = {
+    ["Discord"] = {
     	["inv"] =   "tg9uymesSZ",
     	["owner"] = "Woutt#4106",
     },
     
-    ["image"] = {
+    ["Image"] = {
     	["background"] =    "http://www.roblox.com/asset/?id=8211657640",
     	["glow"] =          "http://www.roblox.com/asset/?id=8198465912",
     	["copy"] =          "http://www.roblox.com/asset/?id=8211721462",
